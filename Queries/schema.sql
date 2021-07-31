a7402a862e5b02ebc33c74df51decf94b6427f54
Ellen Rafferty <ellenrafferty@Ellens-MacBook-Pro-2.local>
2021-07-31 14:00:43 -0700

    Adding retirement infor queries

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,3 +1,9 @@
+DROP TABLE departments CASCADE;
+DROP TABLE employees CASCADE;
+DROP TABLE dept_manager;
+DROP TABLE salaries;
+DROP TABLE titles;
+DROP TABLE dept_emp;
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
@@ -9,6 +15,7 @@ CREATE TABLE departments (
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
@@ -40,7 +47,7 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 CREATE TABLE dept_emp (
@@ -53,4 +60,22 @@ CREATE TABLE dept_emp (
 	PRIMARY KEY (emp_no, dept_no)
 );
 
-SELECT * FROM departments;
+	
+SELECT * FROM titles;
+
+-- Importing Data
+
+COPY departments FROM '/Users/ellenrafferty/Public/departments.csv' DELIMITER ',' CSV HEADER;
+
+COPY employees FROM '/Users/ellenrafferty/Public/employees.csv' DELIMITER ',' CSV HEADER;
+
+COPY dept_manager FROM '/Users/ellenrafferty/Public/dept_manager.csv' DELIMITER ',' CSV HEADER;
+
+COPY salaries FROM '/Users/ellenrafferty/Public/salaries.csv' DELIMITER ',' CSV HEADER;
+
+COPY titles FROM '/Users/ellenrafferty/Public/titles.csv' DELIMITER ',' CSV HEADER;
+
+COPY dept_emp FROM '/Users/ellenrafferty/Public/dept_emp.csv' DELIMITER ',' CSV HEADER;
+
+
+
